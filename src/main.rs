b3c12b5cb0e8ce509ce61230bbab5de45b161672
cecarbs <carbonel257@gmail.com>
2023-12-30 14:22:14 -0600

    Added todos

--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,10 @@ use std::{
     process::Command,
 };
 
+// TODO:
+// 1. add pipe operation
+// 2. interupt a signal but not interrupt program (run 'cat' and press ctrl-c)
+// 3. add command history to shell
 fn main() {
     loop {
         // Prompt
