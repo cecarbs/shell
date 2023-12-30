a0e3d59a6e9a82c3c87c6ca9cab83f98147904dd
cecarbs <carbonel257@gmail.com>
2023-12-30 13:58:14 -0600

    Implemented 'cd' and 'exit' commands

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,47 @@
+use std::{
+    env,
+    io::{stdin, stdout, Write},
+    path::Path,
+    process::Command,
+};
+
 fn main() {
-    println!("Hello, world!");
+    loop {
+        // Prompt
+        print!("> ");
+        // ensure it prints before calling read_line
+        stdout().flush().unwrap();
+
+        let mut input = String::new();
+        stdin().read_line(&mut input).unwrap();
+
+        let mut parts = input.split_whitespace();
+        let command = parts.next().unwrap();
+        let args = parts;
+
+        match command {
+            "cd" => {
+                // default to '/' as new directory if one was not provided
+                let new_dir = args.peekable().peek().map_or("/", |x| *x);
+                let root = Path::new(new_dir);
+                if let Err(e) = env::set_current_dir(root) {
+                    eprintln!("{}", e);
+                }
+            }
+            "exit" => return,
+            command => {
+                let mut child = Command::new(command).args(args).spawn().unwrap();
+
+                child.wait().unwrap();
+            }
+        }
+
+        // // read_line leaves a trailing newline, which trim removes
+        // let command: &str = input.trim();
+        //
+        // let mut child = Command::new(command).spawn().unwrap();
+        //
+        // // complete task before accepting another command
+        // child.wait().unwrap();
+    }
 }
